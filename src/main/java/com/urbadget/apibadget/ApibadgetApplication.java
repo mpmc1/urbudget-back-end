b65bbb3d8627877f7166b406d57d1e20dc758249
Carlos Puerta <puertam19@gmail.com>
2023-02-15 20:50:51 -0500

    Springboot initializr files

---
+++ src/main/java/com/urbadget/apibadget/ApibadgetApplication.java
@@ -0,0 +1,13 @@
+package com.urbadget.apibadget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibadgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibadgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbadget/apibadget/ApibadgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbadget.apibadget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibadgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
