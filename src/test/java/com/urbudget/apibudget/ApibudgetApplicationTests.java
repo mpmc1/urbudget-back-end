0c06d01013a2367ed6928f78aeb58cc451923878
Carlos Puerta <puertam19@gmail.com>
2023-02-15 21:04:51 -0500

    Changed names

---
+++ src/test/java/com/urbudget/apibudget/ApibudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbudget.apibudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
