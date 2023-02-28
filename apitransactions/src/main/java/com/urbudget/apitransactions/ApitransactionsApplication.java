be747e6b66f13bcde06d92461badf671c9246508
Carlos Puerta <puertam19@gmail.com>
2023-02-27 21:02:47 -0500

    Transactions added

---
+++ apitransactions/src/main/java/com/urbudget/apitransactions/ApitransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.urbudget.apitransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitransactionsApplication.class, args);
+	}
+
+}
---
+++ apitransactions/src/test/java/com/urbudget/apitransactions/ApitransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbudget.apitransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
