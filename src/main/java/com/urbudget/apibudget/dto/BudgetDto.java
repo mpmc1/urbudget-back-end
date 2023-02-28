4ef34f003d7da0cf762b95eeafdf228d6dcabfbc
Carlos Puerta <puertam19@gmail.com>
2023-02-27 20:58:44 -0500

    Transactions added

--- src/main/java/com/urbudget/apibudget/dto/BudgetDto.java
+++
@@ -1,8 +0,0 @@
-package com.urbudget.apibudget.dto;
-
-public class BudgetDto {
-    private YearDto year;
-    private float value;
-    private float monthIncomes;
-    private float monthOutcomes;
-}
