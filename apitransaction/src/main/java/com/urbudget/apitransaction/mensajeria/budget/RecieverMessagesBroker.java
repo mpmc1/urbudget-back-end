e02330dd0c29f44e4c30048b275d0fab6a7f24b6
Papablol <juancho.carmona.13@gmail.com>
2023-04-24 21:10:29 -0500

    Broker Reciever

---
+++ apitransaction/src/main/java/com/urbudget/apitransaction/mensajeria/budget/RecieverMessagesBroker.java
@@ -0,0 +1,35 @@
+package com.urbudget.apitransaction.mensajeria.budget;
+
+import com.urbudget.apitransaction.domain.budget.Budget;
+import com.urbudget.apitransaction.service.budget.BudgetService;
+import com.urbudget.apitransaction.util.gson.MapperJsonObjeto;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+@Component
+public class RecieverMessagesBroker {
+    @Autowired
+    private BudgetService budgetService;
+
+    private final MapperJsonObjeto mapperJsonObjeto;
+    public RecieverMessagesBroker(MapperJsonObjeto mapperJsonObjeto) {
+        this.mapperJsonObjeto = mapperJsonObjeto;
+
+    }
+
+    @RabbitListener(queues = "${budget.queue-recibir.budget.queue-name}")
+    public void receiveMessageProcessClient(String message) {
+        try {
+            budgetService.save(obtenerObjetoDeMensaje(message).get());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    private Optional<Budget> obtenerObjetoDeMensaje(String mensaje) {
+        return mapperJsonObjeto.ejecutar(mensaje, Budget.class);
+    }
+
+
+}
